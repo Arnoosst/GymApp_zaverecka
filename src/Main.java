718115c93f6a090e2d400ae8a5c38452c8e4ad91
malin <malinek.vojta@gmail.com>
2025-05-25 10:18:28 +0200

    Remove outdated TODO comment on unit tests

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
         //TODO v app frame a v modelu ve 2 tridach, v wokroutinfoframe, projit workout execution (toz uz je v planu)
         //TODO projit vlaste k tomu to sedi selectworkoutpanel
-        //TODO jinak jeste ty unitTesty
         //TODO predelani executionWorkout
 
         //TODO predelani userpanelu refreshu take zkusit to
